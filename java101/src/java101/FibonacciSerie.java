d46fe82db57f0dcb381cfc6e40fbfc2087614321
barkin <c.barkinozer@gmail.com>
2021-07-18 17:32:01 +0300

    FibonacciSerie Done

---
+++ java101/src/java101/FibonacciSerie.java
@@ -0,0 +1,25 @@
+package java101;
+
+import java.util.Scanner;
+
+public class FibonacciSerie {
+	public static void main(String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int n1 = 0, n2 = 1, n3, i, n;
+		
+		System.out.print("N Sayýsýný Girin:");
+		n = scanner.nextInt();
+		
+		System.out.print(n1 + " " + n2);
+		
+		// Döngü 2 den baþlýyor çünkü ilk 2 terim 0 ve 1 her zaman yazýlacak
+		for (i = 2; i < n; ++i) {
+			n3 = n1 + n2;
+			System.out.print(" " + n3);
+			n1 = n2;
+			n2 = n3;
+		}
+		System.out.println();
+	}
+}
