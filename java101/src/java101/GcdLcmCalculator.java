c21819771909f818b29161332020c73734f8e0d1
barkin <c.barkinozer@gmail.com>
2021-07-20 13:44:36 +0300

    Quick fix : scanner's closed

--- java101/src/java101/GcdLcmCalculator.java
+++ java101/src/java101/GcdLcmCalculator.java
@@ -10,6 +10,7 @@ public class GcdLcmCalculator {
 		n1=scanner.nextInt();
 		System.out.println("Enter n2");
 		n2=scanner.nextInt();
+		scanner.close();
 		int ebob=1;
 		for(int i=1;i<=n1;i++) {
 			if(n1%i==0&&n2%2==0) {
