ef6a2641d21b8edb08cba50b3cba4151ed953702
barkin <c.barkinozer@gmail.com>
2021-07-20 19:12:00 +0300

    ArrayHarmonicMeanCalculator Done

---
+++ java101/src/java101/ArrayHarmonicMeanCalculator.java
@@ -0,0 +1,13 @@
+package java101;
+
+public class ArrayHarmonicMeanCalculator {
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 5};
+		double sum = 0;
+		double n= numbers.length;
+		for (int i = 0; i < n; i++) {
+            sum += (1/numbers[i]);
+		}
+        System.out.println(n/sum);
+	}
+}
