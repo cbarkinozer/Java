240afbcee22911c69ec34ede04df31637e4db253
barkin <c.barkinozer@gmail.com>
2021-07-25 13:11:37 +0300

    adventureGame class added,Samurai class created

---
+++ java102/src/adventureGame/GameCharacters/Samurai.java
@@ -0,0 +1,11 @@
+package adventureGame.GameCharacters;
+
+import adventureGame.GameCharacter;
+
+public class Samurai extends GameCharacter {
+
+	public Samurai(int damage, int health, int money) {
+		super(damage, health, money);
+	}
+
+}
