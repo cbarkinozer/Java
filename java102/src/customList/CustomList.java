7a5b8625fb78191be35bbc59b44eb891c739cf52
barkin <c.barkinozer@gmail.com>
2021-07-27 20:14:46 +0300

    CustomList done, next step is testing.

--- java102/src/customList/CustomList.java
+++ java102/src/customList/CustomList.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class CustomList<T> {
 
-	private int size=0;
+	private int size = 0;
 	private static final int DEFAULT_CAPACITY = 10;
-	
-	//Important: This array will store all elements added to list
+
+	// Important: This array will store all elements added to list
 	private Object elements[];
 
 	public CustomList(int capacity) {
@@ -19,75 +19,121 @@ public class CustomList<T> {
 
 		elements = new Object[DEFAULT_CAPACITY];
 	}
-	
+
 	public int size() {
 		return size;
 	}
-	
-	
+
 	public int getCapacity() {
 		return elements.length;
 	}
-	
+
 	public void add(T data) {
 		if (size == elements.length) {
 			int newSize = elements.length * 2;
-			//Changes arrays size
-	        elements = Arrays.copyOf(elements, newSize);
-        }
-        elements[size++] = data;
+			// Changes arrays size
+			elements = Arrays.copyOf(elements, newSize);
+		}
+		elements[size++] = data;
 	}
-	
-	
+
 	@SuppressWarnings("unchecked")
 	public T get(int index) {
 		if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
-        }
-        return (T) elements[index];
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+		}
+		return (T) elements[index];
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public T remove(int index) {
 		if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
-        }
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+		}
 		Object item = elements[index];
-        int numberElements = elements.length - ( index + 1 ) ;
-        System.arraycopy( elements, index + 1, elements, index, numberElements ) ;
-        size--;
-        return (T) item;
+		int numberElements = elements.length - (index + 1);
+		System.arraycopy(elements, index + 1, elements, index, numberElements);
+		size--;
+		return (T) item;
 	}
-	
+
 	public void set(int index, T data) {
 		if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
-        }
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+		}
 		remove(index);
 		elements[index] = data;
-		
-		
+
 	}
+
 	public String toString() {
-		 StringBuilder sb = new StringBuilder();
-         sb.append('[');
-         for(int i = 0; i < size ;i++) {
-             sb.append(elements[i].toString());
-             if(i<size-1){
-                 sb.append(",");
-             }
-         }
-         sb.append(']');
-         return sb.toString();
+		StringBuilder sb = new StringBuilder();
+		sb.append('[');
+		for (int i = 0; i < size; i++) {
+			sb.append(elements[i].toString());
+			if (i < size - 1) {
+				sb.append(",");
+			}
+		}
+		sb.append(']');
+		return sb.toString();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public int indexOf(T data) {
+		for (int i = 0; i < size; i++) {
+			if(data==elements[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public int lastIndexOf(T data) {
+		for (int i = size; i >0; i--) {
+			if(data==elements[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public T[] toArray() {
+		Object array = Arrays.copyOf(elements, size);
+		return (T[]) array;
+	}
+
+	public void clear() {
+		Arrays.fill(elements, null);
+	}
+
+	public CustomList<T> subList(int start, int finish) {
+		if (start >= size || start < 0 || finish >= size || finish < 0) {
+			throw new IndexOutOfBoundsException("Start: " + start +"Finish: "+finish+ ", Size " + size);
+		}
+		final CustomList<T> result=null;
+		int numberElements = finish-start;
+		System.arraycopy(elements, start + 1, result, finish, numberElements);
+		
+		return result;
+	}
+
+	boolean contains(T data) {
+
+		for (int i = 0; i < size; i++) {
+			if(data==elements[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
--- java102/src/customList/Main.java
+++ java102/src/customList/Main.java
@@ -2,43 +2,79 @@ package customList;
 
 public class Main {
 	public void main(String[] args) {
-		
-		//Tests
-		
-		//Test serie 1
+
+		// Tests
+
+		// Test serie 1
 		CustomList<Integer> liste = new CustomList<>();
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
-        liste.add(10);
-        liste.add(20);
-        liste.add(30);
-        liste.add(40);
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
-        liste.add(50);
-        liste.add(60);
-        liste.add(70);
-        liste.add(80);
-        liste.add(90);
-        liste.add(100);
-        liste.add(110);
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
-        
-        //Test serie 2
-        CustomList<Integer> liste2 = new CustomList<>();
-        liste2.add(10);
-        liste2.add(20);
-        liste2.add(30);
-        System.out.println("2. indisteki değer : " + liste2.get(2));
-        liste2.remove(2);
-        liste2.add(40);
-        liste2.set(0, 100);
-        System.out.println("2. indisteki değer : " + liste2.get(2));
-        System.out.println(liste2.toString());
-        
-        
-        
-        
+		System.out.println("Dizideki Eleman Sayısı : " + liste.size());
+		System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+		liste.add(10);
+		liste.add(20);
+		liste.add(30);
+		liste.add(40);
+		System.out.println("Dizideki Eleman Sayısı : " + liste.size());
+		System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+		liste.add(50);
+		liste.add(60);
+		liste.add(70);
+		liste.add(80);
+		liste.add(90);
+		liste.add(100);
+		liste.add(110);
+		System.out.println("Dizideki Eleman Sayısı : " + liste.size());
+		System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+
+		// Test serie 2
+		CustomList<Integer> liste2 = new CustomList<>();
+		liste2.add(10);
+		liste2.add(20);
+		liste2.add(30);
+		System.out.println("2. indisteki değer : " + liste2.get(2));
+		liste2.remove(2);
+		liste2.add(40);
+		liste2.set(0, 100);
+		System.out.println("2. indisteki değer : " + liste2.get(2));
+		System.out.println(liste2.toString());
+
+		// Test serie 3
+		CustomList<Integer> liste3 = new CustomList<>();
+		System.out.println("Liste Durumu : " + (liste3.isEmpty() ? "Boş" : "Dolu"));
+		liste3.add(10);
+		liste3.add(20);
+		liste3.add(30);
+		liste3.add(40);
+		liste3.add(20);
+		liste3.add(50);
+		liste3.add(60);
+		liste3.add(70);
+
+		System.out.println("Liste Durumu : " + (liste3.isEmpty() ? "Boş" : "Dolu"));
+
+		// Bulduğu ilk indeksi verir
+		System.out.println("Indeks : " + liste3.indexOf(20));
+
+		// Bulamazsa -1 döndürür
+		System.out.println("Indeks :" + liste3.indexOf(100));
+
+		// Bulduğu son indeksi verir
+		System.out.println("Indeks : " + liste3.lastIndexOf(20));
+
+		// Listeyi Object[] dizisi olarak geri verir.
+		Object[] dizi = liste3.toArray();
+		System.out.println("Object dizisinin ilk elemanı :" + dizi[0]);
+
+		// Liste veri türünde alt bir liste oluşturdu
+		CustomList<Integer> mySubList = liste3.subList(0, 3);
+		System.out.println(mySubList.toString());
+
+		// Değerim listedeki olup olmadığını sorguladı
+		System.out.println("Listemde 20 değeri : " + liste3.contains(20));
+		System.out.println("Listemde 120 değeri : " + liste3.contains(120));
+
+		// Listeyi tamamen boşaltır ve varsayılan boyutuna çevirir
+		liste3.clear();
+		System.out.println(liste.toString());
+
 	}
 }
