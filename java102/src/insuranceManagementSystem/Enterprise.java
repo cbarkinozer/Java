d2953a3d47f75b73a229ce8102d463026381392e
barkin <c.barkinozer@gmail.com>
2021-07-30 11:02:35 +0300

    insuranceManagementSystem getters/setters added,quickUpdates

--- java102/src/insuranceManagementSystem/Enterprise.java
+++ java102/src/insuranceManagementSystem/Enterprise.java
@@ -2,4 +2,9 @@ package insuranceManagementSystem;
 
 public class Enterprise extends Account {
 
+	public Enterprise(User user) {
+		super(user);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- java102/src/insuranceManagementSystem/Individual.java
+++ java102/src/insuranceManagementSystem/Individual.java
@@ -2,4 +2,9 @@ package insuranceManagementSystem;
 
 public class Individual extends Account {
 
+	public Individual(User user) {
+		super(user);
+		// TODO Auto-generated constructor stub
+	}
+
 }
