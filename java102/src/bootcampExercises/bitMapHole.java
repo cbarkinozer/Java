d3fb6c980c6ebc5022b670b078d74f291a030415
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-10 19:15:07 +0300

    Update bitMapHole.java

--- java102/src/bootcampExercises/bitMapHole.java
+++ java102/src/bootcampExercises/bitMapHole.java
@@ -1,4 +1,5 @@
 /*
+LEVEL: MEDIUM
 Have the function BitmapHoles(strArr) take the array of strings stored in strArr, which will be a 2D matrix of 0 and 1's, 
 and determine how many holes, or contiguous regions of 0's, exist in the matrix.
 A contiguous region is one where there is a connected group of 0's going in one or more of four directions: up, down, left, or right.
