c415836d41b2eb83bb9c2284139d356c5ccde181
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-13 00:09:15 +0300

    Update groupingTransactionsByItemNames.java

--- java102/src/bootcampExercises/groupingTransactionsByItemNames.java
+++ java102/src/bootcampExercises/groupingTransactionsByItemNames.java
@@ -1,1 +1,26 @@
-/**/
+/*
+-UNFINISHED-
+Level:MEDIUM?
+For a given array of transactions, group all of the transactions by item name. Return an array of strings where each string contains the item name followed by
+a space and the number of associated transactions.
+Note: Sort the array descending by transactions count, the nascending alphabetically by item name for items with matching transaction counts.
+Example:
+transactions=["notebook","notebook","mouse","keyboard","mouse"]
+There are 2 items with 2 transactions each:
+The notebook and mouse in alphabetical order they are mouse notebook.
+There is 1 item with 1 transaction: keyboard.
+The return array sorted as required is ["mouse 2", "keyboard 2", "notebook 1"]
+*/
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static List<String> groupTransactions(List<String>transacitons){
+    }
+
+    public static void main(String[] args){
+        String[] strArr= new String[] {"0011", "0010"} ;
+        System.out.println("Correct Result: 2 . Your Result: "+function(strArr));
+        
+    }
+}
