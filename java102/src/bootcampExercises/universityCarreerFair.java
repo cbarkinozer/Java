4b1e91f22170e3b93412aed03fc95469322aab4f
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-13 00:41:10 +0300

    Update universityCarreerFair.java

--- java102/src/bootcampExercises/universityCarreerFair.java
+++ java102/src/bootcampExercises/universityCarreerFair.java
@@ -1,1 +1,30 @@
+/*
+-UNFINISHED-
+Level:MEDIUM?
+A team organizing the university carrier fair ha sa list of companies along with their respective arrival times and their duration of stay.
+Only one company can present at any time. Given each companies arrival time and the duration they will stay, determine the maximum number
+of presentations that can be hosted during the career fair.
+Example:
+n=5
+arrival=[1,3,3,5,7]
+duration=[2,2,1,2,1]
+The first company arrives at time 1 and stays for 2 hours. At time 3, two companies arrive, but can only one can stay for either 1 or 2 hours.
+The next companies arrive at times 5 and 7 and do not conflict with any others. In total, there can be a maximum 4 promotional event.
 
+Return int maximum number of promotional events that can be hosted.
+Arrival and duration will have equal number of elements.
+
+*/
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static int maxEvents(List<Integer>arrival,List<Integer>duration){
+    }
+
+    public static void main(String[] args){
+        String[] strArr= new String[] {"0011", "0010"} ;
+        System.out.println("Correct Result: 2 . Your Result: "+function(strArr));
+        
+    }
+}
