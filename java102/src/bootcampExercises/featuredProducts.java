84b9567e7d628cd3b4f08e1ccbf046116f40d94c
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-12 23:53:26 +0300

    Update featuredProducts.java

--- java102/src/bootcampExercises/featuredProducts.java
+++ java102/src/bootcampExercises/featuredProducts.java
@@ -1,1 +1,25 @@
+/*
+-UNFINISHED-
+Level:MEDIUM?
+An e-commerce site tracks the purchases made each day. The product that is purchased the most one day is the featured product for the following day.
+If there is a tie for the product purchased most frequently, those product names are ordered alphabetically ascending and the last name in the list is chosen.
+Example
+products=["redShirt","greenPants","redShirt","orangeShoes","blackPants","blackPants"]
+redShirt and blackPants are ordered 2 times in that day. So products are ordered alphabetically.
+redShirt is the last product listed so it is selected.
+Function has a string products[n] array. 1<= n <=10^4.
+Function should return selected product as a string.
+*/
 
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static String featuredProduct(List<String>products){
+    }
+
+    public static void main(String[] args){
+        String[] strArr= new String[] {"0011", "0010"} ;
+        System.out.println("Correct Result: 2 . Your Result: "+function(strArr));
+        
+    }
+}
