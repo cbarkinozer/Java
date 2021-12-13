a0cba5a005fecb922ade526790e2650c1f1a94a2
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-13 17:46:31 +0300

    Update pointsAndLines.java

--- java102/src/bootcampExercises/pointsAndLines.java
+++ java102/src/bootcampExercises/pointsAndLines.java
@@ -12,14 +12,71 @@ Method: 'Vector<Line> getLinesStratingFromPoint(Point p)' which returns the list
 
 import java.util.*;
 import java.io.*;
-public class Main {
- 
-    public static void main(String[] args){
-        String[] strArr= new String[] {"0011", "0010"} ;
-        System.out.println("Correct Result: 2 . Your Result: "+function(strArr));
-        
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Point{
+    private double x;
+    private double y;
+    Point(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+    public double getX(){return x;}
+    public double getY(){return y;}
+    public void setX(double x){this.x=x;}
+    public void setY(double y){this.y=y;}
+    
+    public static double distance(Point p1, Point p2){
+        double distance = Math.sqrt(
+            Math.pow(p2.getY()-p1.getY(),2)+
+            Math.pow(p2.getX()-p1.getX(),2)
+            );
+            
+            return distance;
+    }
+    
+    public String toString(){return "("+this.x+","+this.y+")";}
+}
+class Line{
+    private Point start= new Point(0,0);
+    private Point end= new Point(0,0);
+    
+    Line(Point start, Point end){
+        this.start=start;
+        this.end=end;
+    }
+    public Point getStart(){return start;}
+    public Point getEnd(){return end;}
+    
+    public void setStart(Point start){this.start= start;}
+    public void setEnd(Point end){this.end= end;}
+    
+    public double getLineLength(){return Point.distance(start,end);}
+    
+    public String toString(){return "Start:"+this.start+"End:"+this.end;}
+    
+}
+
+
+interface ListOfLines{
+    public Vector<Line> getLinesFromStartingPoint(Point p);
+    public Line getLineWithMaxLength();
+    public Vector<Line> list_of_lines=null;
+}
+
+
+class LineList implements ListOfLines{
+    public void LineList(){
+    //Code here
     }
 }
-class LineList implements ListOfLines{
-public void LineList(){}
+
+class Main {
+ 
+    public static void main(String[] args){
+        
+        
+    }
 }
