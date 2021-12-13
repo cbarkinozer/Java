4ac9ce2c3afd50cbe18593f1f2b3b7af21fe4185
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-13 16:04:06 +0300

    Update pointsAndLines.java

--- java102/src/bootcampExercises/pointsAndLines.java
+++ java102/src/bootcampExercises/pointsAndLines.java
@@ -1,1 +1,24 @@
-/**/
+/*
+-UNFINISHED-
+Level: MEDIUM?
+Given two fully implemented classes, Point and Line. Read through the given code to understand their implementations.
+Implement the class LineList which implements the interface LineListInterface in order to perform some operations on a list of lines.
+Interface LineListInterface consists of:
+Attribute "Vector<Line>list_of_lines" which stores a list of lines.
+Constructor with parameter Vector<Line> to initialize the list_of_lines attribute.
+Method: 'Line getLineWithMaxLength()' which returns the first longest line from the list of lines.
+*/
+
+import java.util.*;
+import java.io.*;
+public class Main {
+ 
+    public static void main(String[] args){
+        String[] strArr= new String[] {"0011", "0010"} ;
+        System.out.println("Correct Result: 2 . Your Result: "+function(strArr));
+        
+    }
+}
+class LineList implements ListOfLines{
+public void LineList(){}
+}
