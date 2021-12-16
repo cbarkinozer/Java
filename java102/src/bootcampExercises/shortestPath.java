74c82bbc2853ab23b8560e0425d7de06ebe9311c
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-17 00:07:27 +0300

    Update shortestPath.java

--- java102/src/bootcampExercises/shortestPath.java
+++ java102/src/bootcampExercises/shortestPath.java
@@ -19,3 +19,105 @@ Output: A-C-D-F
 Input: new string[] {"4","X","Y","Z","W","X-Y","Y-Z","X-W"}
 Output: X-W
 */
+
+
+import java.util.*; 
+import java.io.*;
+
+public class Main {
+
+  public static String ShortestPath(String[] strArr) {
+    // code goes here
+
+    // unweighted positive-cost graph is provided
+    // find the minimum-cost connection between first and last nodes. 
+
+
+    // Extract information from input
+    int numNodes = Integer.parseInt(strArr[0]);
+    String nodeFirst = strArr[1];
+    String nodeLast = strArr[numNodes];
+    String[] nodesRest = Arrays.copyOfRange(strArr, 2, numNodes);
+    String[] connections = Arrays.copyOfRange(strArr, numNodes + 1, strArr.length);
+
+    // Store possible branches (non-empty)
+    ArrayList<String> branches = new ArrayList<String>();
+    // initial path template added
+    branches.add(nodeFirst);
+
+
+    // Get the initial path template
+    String path = branches.get(0);
+    // Get nodeFirst for iteration
+    String head = String.valueOf(path.charAt(path.length() - 1));
+
+    while(true) {
+      // If any solution exists return it
+      for (String branch : branches) {
+        if (branch.endsWith(nodeLast)) {
+          return branch;
+        }
+      }
+      // Add non-looping possible child paths to the list
+      for (String conn : connections) {
+        // Possible hop
+        String next;
+
+        // Find next possible hop for the head
+        int charIndex = conn.indexOf(head);
+        // Left
+        if (charIndex == 0) {
+          next = String.valueOf(conn.charAt(2));
+        }
+        // Right
+        else if (charIndex == 2) {
+          next = String.valueOf(conn.charAt(2));
+        }
+        // Pass irrelevant connection
+        else {
+          continue;
+        }
+
+        // If next hop will not cause a loop (i.e. it has not been added before)
+        if (path.indexOf(next) < 0) {
+          // Add next as head
+          String child = path + "-" + next;
+          branches.add(0, child);
+        }
+      }
+
+      // Remove the original parent path from the list
+      branches.remove(path);
+
+      // Sort the branches in increasing size
+      if (branches.size() > 0) {
+        branches.sort(Comparator.comparingInt(String::length));
+      }
+      // No possible connections
+      else {
+        return "-1";
+      }
+
+      // Get the least expensive path (might get stuck)
+      path = branches.get(0);
+      // Head node for current minumum-cost path
+      head = String.valueOf(path.charAt(path.length() - 1));
+    }
+  }
+
+
+public static String ShortestPath2(String[] strArr) {
+    //Stream solution
+    return strArr[0];
+}
+  public static void main (String[] args) {
+    // keep this function call here
+    String[] str = {"5", "A", "B", "C","D","F","A-B","A-C","B-C","C-D","D-F"};
+    String[] str2 = {"4", "X", "Y", "Z","W","X-Y","Y-Z","X-W"};
+    
+    System.out.println(ShortestPath(str));
+    System.out.println(ShortestPath(str2));
+    
+  }
+
+}
