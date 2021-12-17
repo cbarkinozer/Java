ca44c13f4b513912207ce551df07938c5a7e8c34
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-17 21:44:11 +0300

    Update featuredProducts.java

--- java102/src/bootcampExercises/featuredProducts.java
+++ java102/src/bootcampExercises/featuredProducts.java
@@ -12,35 +12,46 @@ Function should return selected product as a string.
 */
 import java.util.*;
 import java.io.*;
+
 public class Main {
+    
     public static String featuredProduct(List<String>products){
         Map<String,Integer> map= new HashMap<>();
         for(String str: products){
+            
             if(map.get(str)!=null){ //If there are elements
                 map.put(str,map.get(str)+1);
             }else{
                 map.put(input,1); //If there are elements key is 1
             }
         }
-            int maxVal = Collections.max(map.values();
+            
+            
+            int maxVal = Collections.max(map.values());
             ArrayList<Integer> num= new ArrayList<>();
+            
             //Iterate hashmap
-            for(Entry<String,Integer> entry: map.entrySet()){
+            for(HashMap<String,Integer> entry: map.entrySet()){
+                
                 if(entry.getValue()==maxVal){
                     num.add(entry.getKey());
                 }
+                
                 if(num.size()==1){
                     return entry.getKey();
                 }else if(num.size()>1){
                     Collections.sort(products);
                     return products.get(products.size()-1);
                 }
+                
             }
-        }
-    }
+    
+    
+    
+}
     public static String featuredProduct2(List<String>products){
         //Stream solution
-        return products[0];
+        return products.get(0);
     }
     public static void main(String[] args){
         
