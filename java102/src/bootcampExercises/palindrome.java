1bd07f1de4fcf3086d222c5c166b2f1d1f08d5af
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-23 13:51:38 +0300

    Create palindrome.java

---
+++ java102/src/bootcampExercises/palindrome.java
@@ -0,0 +1,14 @@
+/*
+-UNFINISHED-
+LEVEL:EASY
+Palindrome
+Have the function Palindrome(str) take the str parameter being passed and return the string true if the parameter is a palindrome,
+(the string is the same forward as it is backward) otherwise return the string false. For example: "racecar" is also "racecar" backwards.
+Punctuation and numbers will not be part of the string.
+Examples
+Input: "never odd or even"
+Output: true
+Input: "eye"
+Output: true
+*/
+
