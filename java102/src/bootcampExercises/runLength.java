738529f7520cdc9c35ef767fe43f81bd34da780a
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-23 13:52:50 +0300

    Create runLength.java

---
+++ java102/src/bootcampExercises/runLength.java
@@ -0,0 +1,13 @@
+/*
+-UNFINISHED-
+LEVEL:MEDIUM
+Run Length
+Have the function RunLength(str) take the str parameter being passed and return a compressed version of the string using the Run-length encoding algorithm.
+This algorithm works by taking the occurrence of each repeating character and outputting that number along with a single character of the repeating sequence.
+For example: "wwwggopp" would return 3w2g1o2p. The string will not contain any numbers, punctuation, or symbols.
+Examples
+Input: "aabbcde"
+Output: 2a2b1c1d1e
+Input: "wwwbbbw"
+Output: 3w3b1w
+*/
