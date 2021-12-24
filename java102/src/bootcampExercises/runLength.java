9e8f179fbd9b6bb7135b78636a7ed493ad355345
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-24 10:28:07 +0300

    Update runLength.java

--- java102/src/bootcampExercises/runLength.java
+++ java102/src/bootcampExercises/runLength.java
@@ -11,3 +11,36 @@ Output: 2a2b1c1d1e
 Input: "wwwbbbw"
 Output: 3w3b1w
 */
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static String runLength(String str){
+      int count=1;
+      ArrayList<Character> ch = new ArrayList<Character>();
+      for(char c: str.toCharArray()){
+        ch.add(c);
+      }
+      ArrayList<Character> ch2 = new ArrayList<Character>();
+      for(int i=0;i<ch.size();i++){
+        count=1;
+        for(int j=i;j<ch.size();j++){
+          if(ch.get(i).equals(ch.get(j))){
+            count++;
+          }else{
+            ch2.add(String.valueOf(count)).charAt(0));
+            ch2.add(ch.get(i));
+            break;
+          }
+        }
+      }
+    StringBuilder sb= new StringBuilder();
+    for(Character c:ch2){
+      sb.append(c);
+    }
+    return sb.toString();
+    }
+    public static void main(String[] args){
+      Scanner scanner = new Scanner(System.in);
+      System.out.println(runLength(scanner.nextLine()));
+    }
+}
