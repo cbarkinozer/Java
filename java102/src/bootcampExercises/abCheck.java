7ba7a82d349f582055054937513e68c44d5151e4
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-30 12:03:50 +0300

    Update abCheck.java

--- java102/src/bootcampExercises/abCheck.java
+++ java102/src/bootcampExercises/abCheck.java
@@ -1,6 +1,14 @@
 /*
 UNFINISHED
 LEVEL:EASY
+AB Check
+Have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere
+in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false.
+Examples
+Input: "after badly"
+Output: false
+Input: "Laura sobs"
+Output: true
 */
 
 /*
