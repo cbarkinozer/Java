6ad6ef060ee3dc4bfee102b57faf8a01cff747b9
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-01 12:45:43 +0300

    Update abCheck.java

--- java102/src/bootcampExercises/abCheck.java
+++ java102/src/bootcampExercises/abCheck.java
@@ -31,33 +31,28 @@ TEST CASES
 10. For input "bzzza" the output was incorrect. The correct output is true
 */
 
-
-import java.util.*; 
-import java.io.*;
-import java.math.*;
+import java.util.; 
+import java.io.;
 
 class Main {
 
   public static String ABCheck(String str) {
-    str=str.replace(" ","");
-    int count=0;
-    int count2=0;
-    int index=str.indexOf('a');
-    int index2=str.indexOf('b');
-    count= Math.abs(str.indexOf('a',index+1)-str.indexOf('b'));
-    count2=Math.abs(str.indexOf('b',index2+1)-str.indexOf('a'));
-    if(count==3 || count2==3){
-      return "true";
-    }else if(count!=3 && count2 !=3){
-      return "false";
-    } else{
-      return "error";
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == 'a'  str.charAt(i) == 'b') {
+        int count = 0;
+        for (int j = i+1; j < str.length(); j++) {
+          if (((str.charAt(j) == 'b' && str.charAt(i) == 'a')  (str.charAt(j) == 'a' && str.charAt(i) == 'b')) && count == 3) {
+            return "true";
+          }
+          count++;
+        }
+      }
     }
+    return "false";
   }
 
-  public static void main (String[] args) {  
-    // keep this function call here     
-    Scanner s = new Scanner(System.in);
+  public static void main (String[] args) { 
+Scanner s = new Scanner(System.in);
     System.out.print(ABCheck(s.nextLine())); 
   }
 
