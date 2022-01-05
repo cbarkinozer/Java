295cc14f15831fef783be25c7ef0e243d1c31141
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-05 13:49:06 +0300

    Update consonantCount.java

--- java102/src/bootcampExercises/consonantCount.java
+++ java102/src/bootcampExercises/consonantCount.java
@@ -1,3 +1,30 @@
 /*
 Level: EASY
+Consonant Count
+Have the function ConsonantCount(str) take the str string parameter being passed and return the number of consonants the string contains.
+Examples
+Input: "Hello World"
+Output: 7
+Input: "Alphabets"
+Output: 6
 */
+
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static String ConsonantCount(String str) {
+    str= str.replaceAll("[aeiou AEIOU]","");
+    StringBuffer strbuff = new StringBuffer(str);
+    int len = strbuff.length();
+    return String.valueOf(len);
+  }
+
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(ConsonantCount(s.nextLine())); 
+  }
+
+}
