949de3cba9bdbd4b8654b9a5d540ea8c3062fc8e
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-05 13:50:37 +0300

    Update primeTime.java

--- java102/src/bootcampExercises/primeTime.java
+++ java102/src/bootcampExercises/primeTime.java
@@ -1,3 +1,11 @@
 /*
 Level: Medium
+Prime Time
+Have the function PrimeTime(num) take the num parameter being passed and return the string true if the parameter is a prime number, otherwise return the string false. 
+The range will be between 1 and 2^16.
+Examples
+Input: 19
+Output: true
+Input: 110
+Output: false
 */
