616f6240a089bc8a86f89ef6fba4250f4f19b08e
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-05 13:58:30 +0300

    Update primeTime.java

--- java102/src/bootcampExercises/primeTime.java
+++ java102/src/bootcampExercises/primeTime.java
@@ -2,7 +2,7 @@
 Level: Medium
 Prime Time
 Have the function PrimeTime(num) take the num parameter being passed and return the string true if the parameter is a prime number, otherwise return the string false. 
-The range will be between 1 and 2^16(65536).
+The range will be between 1 and 2^16(65536). 65521 is a prime.
 Examples
 Input: 19
 Output: true
