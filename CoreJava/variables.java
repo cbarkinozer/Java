2b57210ae36a2895eb1ce3690121aba6ca4c8517
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-10 18:25:28 +0300

    Update variables.java

--- CoreJava/variables.java
+++ CoreJava/variables.java
@@ -1,4 +1,4 @@
-import.java.util.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -40,12 +40,42 @@ public class Main {
         System.out.println("You can write what you want it to print");
         System.out.println(value); //Or you can just print a value
         System.out.println(str+value); //Printing values together
+        
+        
+
+
         //Basic arithmetic operations
         int value2=5;
         value= value + value2;
         value = value - value2;
         value= value * value2;
         value = value / value2;
+        value = value % value2; //Reminder operation(divides and gets the reminder).
+        
+        //There are some shortcuts for arithmetic operations that are done on the same variable.
+        a=a+1;
+        //Also can be done like following
+        a+=1;
+        //Also like this
+        a++;
+        //Or
+        ++a;
+        //++a and a++ are not the same if used in the same sentences.
+        //These are caled prefix and postfix.
+        //Think them as increment and print value, print and increment value.
+        
+        //Also for other operations
+        a-=1;
+        a*=1;
+        a/=1;
+        
+        
+        // "+" operation is also does string concatination
+        str=str+", Welcome";
+        //You can put special commands inside strings to perform several operations.
+        //Such as \t is for tab, and \n for new line etc.
+        System.out.println(str);
+        System.out.println("\n"+str+"\n");
         
         //For more complex operations there are predefined classes such as Math.
         //You can import them using "import" keyword at the beginning of the file.
@@ -69,6 +99,101 @@ public class Main {
         byte halfBt = (byte)(bt/2);
         
         
+        //How to make 2 variables change their values  ?
+        int temp=0;
+        int var1=10;
+        int var2=20;
+        System.out.println("Before swap: "+var1+" and "+var2);
+        temp=var2;
+        var2=var1;
+        var1=temp;
+        System.out.println("After swap: "+var1+" and "+var2);
+        
+
+        //Try these exercises : calculate BMI and calculate hypotenuse
+        
+       
+        
+        //Getting input from user
+        Scanner scanner = new Scanner(System.in); 
+        //Scanner (import java.util) gets input from System.in
+        System.out.println("Enter integer");
+        int var = scanner.nextInt(); //Read next int
+        //nextLine() reads stirng, next() reads non-space value
+        //You can read any other primitive type as well.
+        System.out.println("You entered integer: "+var);
+        
+        
+        //Conditional state structures
+        if(scanner.hasNextLine()){ //If scanner has next value
+            String string = new String();
+            string= scanner.nextLine();//Initializing string as input
+            System.out.println("You entered following String:"+ string); //Print input
+        }else{
+            System.out.println("Please enter a String");
+        }
+       
+        
+        /*
+        Be aware if  value in if skips 0 it can not catch.
+        Also floating points such as float and double can not be equal to a given value
+        They have precision error. Do not do if( double == 2.5){}. You can prefer
+        greater > or smaller < symbols.
+        Also String value can not be equal because == operation checks if they have
+        The same memory place. You have to use str.equals() or str.equalsIgnoreCase()
+        which are predefined methods for String. "abc"=="abc" is legal. str1==str2 is not.
+        */
+        
+        //For loop
+        for(int z=0;z<10;z++){ //Generally i is used as index
+            if(z%2==0){
+                continue;
+            }
+            System.out.println("Hello floppa"+z);
+            if(z==7){
+                break;
+            }
+        }
+        
+        //While
+        int variable3 =0;
+        while (variable3 <10){
+            variable3++;
+        } 
+        //Or
+        while (variable3 !=0){
+            variable3--;
+        }
+        
+        //Do-while first runs it than checks. Only difference between do-while and
+        //while is  that extra 1 run at the beginning.
+        int until=0;
+        do{
+            until++;
+        }while(until !=10);
+        
+        
+        //Switch
+            int day = 2;
+            switch (day) {
+                case 1:
+                    System.out.println("1. door Mr. Gill");
+                    break;
+                case 2:
+                    System.out.println("2. door Mrs. Roy");
+                    break;
+                case 3:
+                    System.out.println("3. door Mr. Jenkins");
+                    break;    
+
+            }
+          /*
+        There are break and continue. You can see break in switch at the top.
+        Break breaks the decision structure it is in.
+        Continue skips that loop. You can also define a label and jump to that label.
+        Using break. These break and continue structures are not prefered often 
+        because they are error prone.
+        */
         
         
     }
