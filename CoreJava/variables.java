b147380cd708869f589f8c84c280894dbfc5ca56
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-10 17:03:49 +0300

    Update variables.java

--- CoreJava/variables.java
+++ CoreJava/variables.java
@@ -4,14 +4,16 @@ public class Main {
     public static void main(String[] args) {
         //In java there are reference types and primitive types
         //Following are primitive types
+        //long>int>short>byte
         int a=1; //int is generally prefered
         long b=1; //long is good when you transform integer value to double
         short c=1; //Prefered for limited integer values
-        byte d=1;   //Holds 8bit data. Bit is binary value either 1 or 0.
-        float e=1;  //Holds floating points. Do not hold exact value (has precision error).
-        double f=1.0; //Generally double is prefered over float. More precise than float. 
+        byte d=1;   //Holds 8bit data(-128,127). Bit is binary value either 1 or 0.
+        float e=1.2f;  //Holds floating points. Do not hold exact value (has precision error).
+        double f=1.2; //Generally double is prefered over float. More precise than float. 
         boolean g=true; //Binary data encoded as true(1) and false(0).
-        char h='A'; //char is for doing ascii operations.
+        char h='A'; //char is for doing unicode operations.
+        char i='\u0152';
         /*
         Use String for character operations.
         There are no unsigned values in java.
@@ -47,6 +49,25 @@ public class Main {
         
         //For more complex operations there are predefined classes such as Math.
         //You can import them using "import" keyword at the beginning of the file.
+        
+        System.err.println(Integer.MIN_VALUE); //-2^31    (~-2million)
+        System.err.println(Integer.MAX_VALUE); //2^31 - 1 (~2million)
+        
+        //Integer != int. Java is case sensitive and these primitive named classes
+        //are wrapper classes. They represent primitive types but are reference types.
+        //These reference types also has pre-defined methods for programmers to use.
+        
+        short j=1000;
+        //When an arithmetic operation is done between two different primitive types,
+        //result becomes as the type of the larger type.
+        int k= (j/2); //Translates value because the translated type has larger space.
+        //Which means no data loss.
+        
+        byte bt=100;
+        //byte halfBt=(bt/2); Because there might be data loss, Java does not convert
+        //Automatically but you can do it (by accepting the possible value loss).
+        byte halfBt = (byte)(bt/2);
+        
         
         
         
