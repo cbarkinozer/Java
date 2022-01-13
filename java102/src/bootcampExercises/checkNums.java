d13aa260b4e556f74f10f05743ee7bbc446c4c67
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-13 12:23:36 +0300

    Update checkNums.java

--- java102/src/bootcampExercises/checkNums.java
+++ java102/src/bootcampExercises/checkNums.java
@@ -1,3 +1,28 @@
 /*
   level:EASY
+Check Nums
+Have the function CheckNums(num1,num2) take both parameters being passed and return the string true if num2 is greater than num1, otherwise return the string false.
+If the parameter values are equal to each other then return the string -1.
+Examples
+Input: 3 & num2 = 122
+Output: true
+Input: 67 & num2 = 67
+Output: -1
 */
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static int CheckNums(int num1, int num2) {
+    // code goes here  
+    return num1;
+  }
+
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(CheckNums(s.nextLine())); 
+  }
+
+}
