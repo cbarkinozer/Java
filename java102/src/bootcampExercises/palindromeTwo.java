063872c4d6b2e1348de60705b545ced406377b1c
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-13 12:28:26 +0300

    Update palindromeTwo.java

--- java102/src/bootcampExercises/palindromeTwo.java
+++ java102/src/bootcampExercises/palindromeTwo.java
@@ -1,3 +1,12 @@
 /*
 level:MEDIUM
+Have the function PalindromeTwo(str) take the str parameter being passed and return the string true if the parameter is a palindrome,
+(the string is the same forward as it is backward) otherwise return the string false.
+The parameter entered may have punctuation and symbols but they should not affect whether the string is in fact a palindrome. For example:
+"Anne, I vote more cars race Rome-to-Vienna" should return true.
+Examples
+Input: "Noel - sees Leon"
+Output: true
+Input: "A war at Tarawa!"
+Output: true
 */
