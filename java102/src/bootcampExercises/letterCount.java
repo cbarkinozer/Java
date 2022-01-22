2997fc51089a3ed35b18d0a957772b7c9d85b38b
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-22 20:26:39 +0300

    Update letterCount.java

--- java102/src/bootcampExercises/letterCount.java
+++ java102/src/bootcampExercises/letterCount.java
@@ -9,29 +9,29 @@ Output: Hello
 Input: "No words"
 Output: -1
 
-1. For input "no words here" the output was incorrect. The correct output is here
-2. For input "I lied before" the output was incorrect. The correct output is before
-3. For input "coderbyte" the output was incorrect. The correct output is coderbyte
-4. For input "yellow and red" the output was incorrect. The correct output is yellow
-5. For input "a b c d ee" the output was incorrect. The correct output is ee
+1. For input "Hello apple pie" the output was incorrect. The correct output is Hello. Your output is apple.
+2. For input "yellow and red" the output was incorrect. The correct output is yellow. Your output is red.
 */
 import java.util.*; 
 import java.io.*;
 
-class Main {
+public class Main {
 
   public static String LetterCount(String str) {
     int count=0;
+    str +=" "; //adding space at the end to get each word witg "ch !=' '" expression
+    int length= str.length();
     Map<Character,Integer> map = new HashMap<Character,Integer>();
     ArrayList<Integer> list = new ArrayList<Integer>();
     Character ch;
-    for(int i=0;i<str.length();i++){
-      ch=str.charAt(i);
+    for(int i=0;i<length;i++){
+      ch=str.charAt(i); // character in str
       if(ch !=' '){ //for each word
-
+        
         if(map.get(ch)==null){ //if character is new add 1
           map.put(ch,1);
         }else{                 //if character already exist
+          
           map.put(ch,map.get(ch)+1); //increase its value
           if(map.get(ch)+1>count){   //if value is the new greatest
             count = map.get(ch)+1;   //save count
@@ -42,24 +42,21 @@ class Main {
         if(count==0){
           count=-1;
         }
+        System.out.println(map.toString());
         list.add(count); //save count for that word
         map.clear();     //empty the map
         count=0;
       } 
     }
     
-    
     ArrayList<Integer> newList = new ArrayList<Integer>();
     newList=list;
     Collections.sort(list);
-    int wordIndex= newList.indexOf(list.get(list.size()-1)); //Show the max word  
-    String[] words = str.split(" ");
-    
+    int wordIndex= newList.indexOf(list.get(list.size()-1)); //Show the max word's index
+    String[] words = str.split(" "); //split words by word
     if(list.get(list.size()-1)==-1){ //If list's greates number is -1 (means all -1)
       return "-1";
     }
-
-
     return words[wordIndex];
   }
 
