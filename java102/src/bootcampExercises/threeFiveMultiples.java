5cf33e93d5f4b04c79e9603922a870d93c28382e
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-26 13:00:11 +0300

    Create threeFiveMultiples.java

---
+++ java102/src/bootcampExercises/threeFiveMultiples.java
@@ -0,0 +1,34 @@
+/*
+Have the function ThreeFiveMultiples(num) return the sum of all the multiples of 3 and 5 that are below num.
+For example: if num is 10, the multiples of 3 and 5 that are below 10 are 3, 5, 6, and 9, and adding them up you get 23, so your program should return 23.
+The integer being passed will be between 1 and 100.
+Examples
+Input: 6
+Output: 8
+Input: 1
+Output: 0
+
+*/
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static int ThreeFiveMultiples(int num) {
+    int count=0;
+    for(int i=2;i<num;i++){
+      if(i%3==0 || i%5==0){
+        count +=i;
+      }
+      
+    }  
+    return count;
+  }
+
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(ThreeFiveMultiples(s.nextLine())); 
+  }
+
+}
