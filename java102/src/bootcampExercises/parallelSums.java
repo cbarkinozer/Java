76397efb5ab617ba15c6de27a1efc0fd2b40f5bd
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-28 16:47:10 +0300

    Update parallelSums.java

--- java102/src/bootcampExercises/parallelSums.java
+++ java102/src/bootcampExercises/parallelSums.java
@@ -19,9 +19,53 @@ import java.io.*;
 class Main {
 
   public static int ParallelSums(int[] arr) {
-    // code goes here  
-    return arr[0];
-  }
+    ArrayList<Integer> liste = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> liste1 = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> value= new ArrayList<ArrayList<Integer>>();
+
+        liste.add(1);
+        liste.add(2);
+        liste.add(3);
+        liste.add(4);
+        String s="";
+        int size=liste.size();
+        int sum =  liste.stream().reduce(0, (i,i1)->{
+            return i+i1;
+        });
+
+        if(size%2!=0) {
+            System.out.println(-1);
+        }
+        else {
+
+            value = sum_up_recursive(liste1,liste,(sum/2),new ArrayList<Integer>());
+            value.get(0).addAll(value.get(1));
+
+            s += value.get(0).toString();
+
+
+        }
+
+        System.out.println(s);
+    }
+
+    static ArrayList<ArrayList<Integer>> sum_up_recursive(ArrayList<ArrayList<Integer>> liste,ArrayList<Integer> numbers, int target, ArrayList<Integer> partial) {
+           int s = 0;
+           for (int x: partial) s += x;
+           if (s == target)
+                liste.add(partial);
+           if (s >= target)
+                return liste;
+           for(int i=0;i<numbers.size();i++) {
+                 ArrayList<Integer> remaining = new ArrayList<Integer>();
+                 int n = numbers.get(i);
+                 for (int j=i+1; j<numbers.size();j++) remaining.add(numbers.get(j));
+                 ArrayList<Integer> partial_rec = new ArrayList<Integer>(partial);
+                 partial_rec.add(n);
+                 sum_up_recursive(liste,remaining,target,partial_rec);
+           }
+           return liste;
+        }
 
   public static void main (String[] args) {  
     // keep this function call here     
