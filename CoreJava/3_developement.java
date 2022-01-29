80d21ebbaaad54b2fbb9b7326f4eae28c0c1c5d3
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-29 13:09:51 +0300

    Update 3_developement.java

--- CoreJava/3_developement.java
+++ CoreJava/3_developement.java
@@ -1,4 +1,10 @@
          //Java Collection's Framework
+         /*
+                 Framework consists of libaries. Libary is predefined methods and classes. Your code calls libaries but your code gets called by framework. 
+                 Java Collection's framework consists of many interfaces and libaries that are used for problems such as grouping and doing operations on similar data.
+                 Collection framework consists list, queue, set, map structures  ( map does not inherits the Collection interface contrary to others).
+         */
+         
          //Exception Handling
         //Java I/O (Input/Output)
         //Multithreading and Concurrency
