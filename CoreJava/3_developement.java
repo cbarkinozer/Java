695d341af030b74ca2658c43ea35d27ea04fd79b
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-29 20:34:55 +0300

    Update 3_developement.java

--- CoreJava/3_developement.java
+++ CoreJava/3_developement.java
@@ -3,6 +3,18 @@
                  Framework consists of libaries. Libary is predefined methods and classes. Your code calls libaries but your code gets called by framework. 
                  Java Collection's framework consists of many interfaces and libaries that are used for problems such as grouping and doing operations on similar data.
                  Collection framework consists list, queue, set, map structures  ( map does not inherits the Collection interface contrary to others).
+                 
+                 //ArrayList
+                 Arrays have fixed amount of element. Arrays are faster. Arrays hold primitive type variables.
+                 ArrayLists hold reference-type variables. There are more predefined methods inside ArrayLists.
+                 Inheritance relationship: ArrayList<List<Collection<Iterable.
+                 
+                 //LinkedList
+                 Elements are NOT stored in contiguous locations, every element is a separate object with 2 parts, data and address of the next node.
+                 ArrayList and LinkedList time complexity comparison:
+                 Note: LinkedList has bigger space-complexity than ArrayList.
+                 When adding to the head of the list: LinkedList is faster. When adding to the tail of the list: ArrayList is faster.
+                 
          */
          
          //Exception Handling
