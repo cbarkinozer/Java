3430ce0233c35b677124f0b4805f7be005d15c39
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-02-06 20:46:53 +0300

    Update 4_MultithreadingDatabasesAndSwing.java

--- CoreJava/4_MultithreadingDatabasesAndSwing.java
+++ CoreJava/4_MultithreadingDatabasesAndSwing.java
@@ -1,5 +1,7 @@
 //Multithreading and Concurrency
-/**/
+/*
+       Concurrency is the simultaneous occurrence of events working at the same time.
+*/
 //JDBC and Mysql Database Operations
 /**/
 //UI programming with Swing
