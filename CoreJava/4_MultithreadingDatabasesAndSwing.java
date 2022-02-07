516bbe3ab05e70ebb0561b593b30536cb0a058e6
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-02-07 12:54:09 +0300

    Update 4_MultithreadingDatabasesAndSwing.java

--- CoreJava/4_MultithreadingDatabasesAndSwing.java
+++ CoreJava/4_MultithreadingDatabasesAndSwing.java
@@ -1,6 +1,13 @@
 //Multithreading and Concurrency
 /*
        Concurrency is the simultaneous occurrence of events working at the same time.
+       Process is every different application running concurrently.
+       Every process has its own memory space in memory, threads share memory space.
+       Thread is different operations on an application running concurrently.
+       Every program has 1 process and 1 thread in that process.
+       Multithreading is faster, gives an opportunity of doing multiple operations but it is harder to program, test,debug.
+       
+       
 */
 //JDBC and Mysql Database Operations
 /**/
