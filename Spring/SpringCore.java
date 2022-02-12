e8071cc329c26f454fcfc0a4abab7fe240606d82
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-02-12 11:46:46 +0300

    Update SpringCore.java

--- Spring/SpringCore.java
+++ Spring/SpringCore.java
@@ -17,3 +17,18 @@ Public class JavaBasedApplication{
   }
 
 }
+
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="
+    http://www.springframework.org/schema/beans
+    http://www.springframework.org/schema/beans/spring-beans.xsd">
+
+    <bean id="dao" class="com.softtech.model.Dao"></bean>
+
+    <bean id="service" class="com.softtech.model.Service">
+        <constructor-arg name="dao" ref="dao"/>
+    </bean>
+
+</beans>
