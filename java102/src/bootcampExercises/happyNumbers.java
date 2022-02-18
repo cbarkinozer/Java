5424a06563d2ba477aafde3683046a1328c470e0
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-02-18 21:53:13 +0300

    Update happyNumbers.java

--- java102/src/bootcampExercises/happyNumbers.java
+++ java102/src/bootcampExercises/happyNumbers.java
@@ -11,11 +11,8 @@ Input: 101
 Output: false
 
 1. For input 5525 the output was incorrect. The correct output is true
-
 2. For input 5255 the output was incorrect. The correct output is true
-
 3. For input 2555 the output was incorrect. The correct output is true
-
 4. For input 5552 the output was incorrect. The correct output is true
 
 */
@@ -24,25 +21,22 @@ import java.io.*;
 
 class Main {
 
-  public static Boolean HappyNumbers(int num) {
-    int count=0;
-    int tempInt=0;
-    String temp = String.valueOf(num);
-    for(int i=0;i<temp.length();i++){
-      tempInt= Integer.parseInt(String.valueOf(temp.charAt(i)));
-      count += Math.pow(tempInt,2);
-      tempInt=0;
-    }
-    if(count==1){
-      return true;
-    }  
-    return false;
+  public static Boolean isHappyNumber(int num) {
+    int total =0;
+   while(num>0){
+    total += (num%10)*(num%10);
+    num /=10;
+   }
+   if(total %10==1||total%10==9){
+    return true;
+   }
+   return false;
   }
 
   public static void main (String[] args) {  
     // keep this function call here     
     Scanner s = new Scanner(System.in);
-    System.out.print(HappyNumbers(s.nextLine())); 
+    System.out.print(isHappyNumber(s.nextLine())); 
   }
 
 }
