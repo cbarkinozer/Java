2bcaef5f69e744943a95b06a4fcef89460069d44
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-03-01 20:03:37 +0300

    Update sudokuQuadrantChecker.java

--- java102/src/bootcampExercises/sudokuQuadrantChecker.java
+++ java102/src/bootcampExercises/sudokuQuadrantChecker.java
@@ -27,132 +27,181 @@ import java.io.*;
 
 class Main {
 
+  
+  public static void main (String[] args) {   
+    Scanner s = new Scanner(System.in);
+    System.out.print(SudokuQuadrantChecker(s.nextLine())); 
+  }
+
+
+  public static String SudokuQuadrantChecker(String[] strArr) {
+    
+    ArrayList<ArrayList<Character>> board = buildBoard(strArr);
+
+    TreeSet<Integer> failedQuadrants = new TreeSet<>();
+
+    for(int i = 0; i < 9; i++){
+      
+      for(int j = 0; j < 9; j++){
+        
+        isFoundInRow(board, i, failedQuadrants);
+        
+        isFoundInColumn(board, j, failedQuadrants);
+        
+        checkBox(board, i-i % 3, j - j%3, failedQuadrants);
+      
+      }
+    
+    }
+
+    StringBuilder stringBuilder = new StringBuilder();
+    
+    for(int i : failedQuadrants){
+      
+      stringBuilder.append(String.format("%d,", i));
+    
+    }
+
+    return stringBuilder.toString().substring(0, stringBuilder.length()-1);
+  
+  }
+
+
   public static ArrayList<ArrayList<Character>> buildBoard(String[] strArr){
+     
      ArrayList<ArrayList<Character>> board = new ArrayList<>();
+     
      for(int i=0; i < strArr.length; i++){
+      
       ArrayList<Character> row = new ArrayList<>();
+      
       for(int j = 0; j < strArr[i].length(); j++){
 
-        if(Character.isDigit(strArr[i].charAt(j)))
-        {
+        if(Character.isDigit(strArr[i].charAt(j))){
+          
           row.add(strArr[i].charAt(j));
-        }
-        else if(strArr[i].charAt(j) == 'x') row.add('x');
+        
+        }else if(strArr[i].charAt(j) == 'x'){
+          row.add('x');
+        } 
+      
       }
 
       board.add(row);
+    
     }
 
     return board;
+  
   }
 
+  
+  public static boolean isFoundInRow(ArrayList<ArrayList<Character>> board, int row, TreeSet<Integer> failed) {
+
+    HashMap<Character, Integer> hMap = new HashMap<>();
+    
+    boolean found = false;
+    
+    for(int i = 0; i < 9; i++){
+      
+      if(hMap.containsKey(board.get(row).get(i))) {
+        
+        failed.add(findQuadrantNumber(row, i));
+        
+        failed.add(hMap.get(board.get(row).get(i)));
+        
+        found = true;
+      
+      }
+      
+      if(board.get(row).get(i) != 'x') {
+        
+        hMap.put(board.get(row).get(i), findQuadrantNumber(row, i));
+        
+      }
+    
+    }
+    
+    return !found;
+  
+  }
+
+
   public static int findQuadrantNumber(int row, int column) {
+    
     int a = row / 3;
+    
     int b = column / 3;
 
     return (a*3) + b + 1;
+  
   }
 
-  public static boolean checkRow(ArrayList<ArrayList<Character>> board, int row, TreeSet<Integer> failed) {
 
-    HashMap<Character, Integer> st = new HashMap<>();
-    boolean found = false;
-    for(int i = 0; i < 9; i++){
-      if(st.containsKey(board.get(row).get(i))) {
-        failed.add(findQuadrantNumber(row, i));
-        failed.add(st.get(board.get(row).get(i)));
-        found = true;
-      }
-if(board.get(row).get(i) != 'x') {
-        st.put(board.get(row).get(i), findQuadrantNumber(row, i));
-        }
-    }
-    return !found;
-  }
+  public static boolean isFoundInColumn(ArrayList<ArrayList<Character>> board, int col,  TreeSet<Integer> failed) {
 
-  public static boolean checkCol(ArrayList<ArrayList<Character>> board, int col,  TreeSet<Integer> failed) {
-
-   HashMap<Character, Integer> st = new HashMap<>();
-    boolean found = false;
+   HashMap<Character, Integer> hMap = new HashMap<>();
+   
+   boolean found = false;
 
     for(int i = 0; i < 9; i++){
-      if(st.containsKey(board.get(i).get(col))){
+      
+      if(hMap.containsKey(board.get(i).get(col))){
+        
         failed.add(findQuadrantNumber(i, col));
-        failed.add(st.get(board.get(i).get(col)));
+        
+        failed.add(hMap.get(board.get(i).get(col)));
 
         found = true;
+      
       }
 
       if(board.get(i).get(col) != 'x') {
-        st.put(board.get(i).get(col), findQuadrantNumber(i,col));
+        
+        hMap.put(board.get(i).get(col), findQuadrantNumber(i,col));
+      
       }
+    
     }
+    
     return !found;
 
     }
-public static boolean checkBox(ArrayList<ArrayList<Character>> board, int startRow, int startCol,  TreeSet<Integer> failed){
-          HashMap<Character, Integer> st = new HashMap<>();
-    boolean found = false;
 
-          for(int row = 0; row < 3; row ++){
-            for(int col=0;col < 3; col++){
+    public static boolean checkBox(ArrayList<ArrayList<Character>> board, int startRow, int startCol,  TreeSet<Integer> failed){
+          
+          HashMap<Character, Integer> hMap = new HashMap<>();
+          
+          boolean found = false;
+
+          for(int row = 0; row < 3; row ++ ){
+            
+            for(int col=0; col < 3; col++ ){
+              
               char curr = board.get(row + startRow).get(col+startCol);
 
-              if(st.containsKey(curr)){
+              if( hMap.containsKey(curr) ){
+                
                 failed.add(findQuadrantNumber(row + startRow, col+startCol));
-                failed.add(st.get(curr));
+                
+                failed.add(hMap.get(curr));
+                
                 found = true;
 
               }
 
               if(curr != 'x'){
 
-               st.put(curr, findQuadrantNumber(row + startRow, col+startCol));
+               hMap.put(curr, findQuadrantNumber(row + startRow, col+startCol));
+              
               }
+            
             }
+          
           }
 
           return true;
     }
 
-
-  public static String SudokuQuadrantChecker(String[] strArr) {
-    ArrayList<ArrayList<Character>> board = buildBoard(strArr);
-
-    TreeSet<Integer> failedQuadrants = new TreeSet<>();
-
-    for(int i = 0; i < 9; i++){
-      for(int j = 0; j < 9; j++){
-        checkRow(board, i, failedQuadrants);
-        checkCol(board, j, failedQuadrants);
-        checkBox(board, i-i % 3, j - j%3, failedQuadrants);
-      }
-    }
-
-    StringBuilder stringBuilder = new StringBuilder();
-    for(int i : failedQuadrants){
-      stringBuilder.append(String.format("%d,", i));
-    }
-
-
-    return stringBuilder.toString().substring(0, stringBuilder.length()-1);
-  }
-
-  public static void main (String[] args) {
-    // keep this function call here
-    Scanner s = new Scanner(System.in);
-    System.out.print(SudokuQuadrantChecker(s.nextLine())); 
-  }
-/*
-  (1,2,3,4,5,6,7,8,9)
-  (x,x,x,x,x,x,x,x,x)
-  (6,x,5,x,3,x,x,4,x)
-  (2,x,1,1,x,x,x,x,x)
-  (x,x,x,x,x,x,x,x,x)
-  (x,x,x,x,x,x,x,x,x)
-  (x,x,x,x,x,x,x,x,x)
-(x,x,x,x,x,x,x,x,x)
-  (x,x,x,x,x,x,x,x,9)"
-  */
 
 }
