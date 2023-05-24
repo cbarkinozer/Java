5d03993928c0119dff78ad6f1c4639774ebea8a6
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2023-05-24 09:41:51 +0300

    Update HowGoodAreYouReally.java

--- StreamSolutions/HowGoodAreYouReally.java
+++ StreamSolutions/HowGoodAreYouReally.java
@@ -10,6 +10,6 @@ import java.util.*;
 
 public class Kata {
   public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
-    return Arrays.stream(classPoints).average().orElse(0)<yourPoints?true:false;
+    return Arrays.stream(classPoints).average().orElse(0) < yourPoints;
   }
 }
