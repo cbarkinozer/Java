750d1e1eeace6e250d84d758eff464da38935f65
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2023-06-08 08:38:05 +0300

    Create TheHighestProfitWins.java

---
+++ StreamSolutions/TheHighestProfitWins.java
@@ -0,0 +1,23 @@
+/*
+Story
+Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples (Input --> Output)
+[1,2,3,4,5] --> [1,5]
+[2334454,5] --> [5,2334454]
+[1]         --> [1,1]
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+*/
+import java.util.Arrays;
+
+class MinMax {
+    public static int[] minMax(int[] arr) {
+        int min = Arrays.stream(arr).min().orElseThrow();
+        int max = Arrays.stream(arr).max().orElseThrow();
+        return new int[]{min, max};
+    }
+}
