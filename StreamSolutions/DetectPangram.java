1eaa2806b288895d99ec365a3ff89695d3534ba6
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2023-06-11 10:15:53 +0300

    Create DetectPangram.java

---
+++ StreamSolutions/DetectPangram.java
@@ -0,0 +1,13 @@
+/*
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+*/
+
+import java.util.stream.Collectors;
+public class PangramChecker {
+  public boolean check(String sentence){
+        return "abcdefghijklmnopqrstuvwxyz".chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .allMatch(sentence.toLowerCase()::contains);
+  }
+}
